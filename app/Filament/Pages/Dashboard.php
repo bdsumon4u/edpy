cf9bbb23d4a86f0b332515ec3e354ef57421fa9c
Sumon Ahmed <bdsumon4u@gmail.com>
2024-05-25 13:06:20 +0600

    wip

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -12,32 +12,32 @@ class Dashboard extends DashboardPage
     public function render(): View
     {
         // cache()->remember('whmcs-sync:'.auth()->id(), now()->addDay(), function () {
-        $data = Http::get(config('services.whmcs.api_endpoint'), [
-            'username' => config('services.whmcs.api_username'),
-            'password' => config('services.whmcs.api_password'),
-            'action' => 'GetClientsProducts',
-            'clientid' => auth()->id(),
-            'responsetype' => 'json',
-        ])
-            ->collect('products.product')
-            ->filter(fn ($product) => $product['groupname'] == 'HotashPay')
-            ->mapWithKeys(function ($product) {
-                $licenseField = current(array_filter($product['customfields']['customfield'], fn ($field) => $field['name'] == 'License Name'));
-                $licenseName = $licenseField ? ($licenseField['value'] ?: $product['name']) : $product['name'];
+            $data = Http::get(config('services.whmcs.api_endpoint'), [
+                'username' => config('services.whmcs.api_username'),
+                'password' => config('services.whmcs.api_password'),
+                'action' => 'GetClientsProducts',
+                'clientid' => auth()->id(),
+                'responsetype' => 'json',
+            ])
+                ->collect('products.product')
+                ->filter(fn ($product) => $product['groupname'] == 'HotashPay')
+                ->mapWithKeys(function ($product) {
+                    $licenseField = current(array_filter($product['customfields']['customfield'], fn ($field) => $field['name'] == 'License Name'));
+                    $licenseName = $licenseField ? ($licenseField['value'] ?: $product['name']) : $product['name'];
 
-                return [$product['id'] => [
-                    'expires_at' => $product['nextduedate'],
-                    'status' => $product['status'],
-                    'key' => $product['domain'],
-                    'id' => $product['id'],
-                    'name' => $licenseName,
-                ]];
-            })
-            ->filter(fn ($product) => $product['key'])
-            ->toArray();
+                    return [$product['id'] => [
+                        'expires_at' => $product['nextduedate'],
+                        'status' => $product['status'],
+                        'key' => $product['domain'],
+                        'id' => $product['id'],
+                        'name' => $licenseName,
+                    ]];
+                })
+                ->filter(fn ($product) => $product['key'])
+                ->toArray();
 
-        Planet::query()->upsert($data, ['id']);
-        $this->planets()->syncWithoutDetaching(array_keys($data));
+            Planet::query()->upsert($data, ['id']);
+            tap(auth()->user())->planets()->syncWithoutDetaching(array_keys($data));
         // });
 
         return parent::render();
