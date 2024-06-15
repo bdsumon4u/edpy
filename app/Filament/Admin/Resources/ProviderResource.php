c99a1690c74455573100c2820562708a8d1f6ee8
Sumon Ahmed <bdsumon4u@gmail.com>
2024-06-15 11:42:11 +0600

    wip

---
+++ app/Filament/Admin/Resources/ProviderResource.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Filament\Admin\Resources;
+
+use App\Filament\Admin\Resources\ProviderResource\Pages;
+use App\Filament\Admin\Resources\ProviderResource\RelationManagers;
+use App\Models\Provider;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ProviderResource extends Resource
+{
+    protected static ?string $model = Provider::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Select::make('type')
+                    ->options([
+                        'Cards' => 'Cards',
+                        'Mobile Banking' => 'Mobile Banking',
+                        'Net Banking' => 'Net Banking',
+                        'Crypto' => 'Crypto',
+                    ])
+                    ->native(false),
+                Forms\Components\TextInput::make('name')
+                    ->required(),
+                Forms\Components\KeyValue::make('meta')
+                    ->reorderable(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->groups([
+                Tables\Grouping\Group::make('type')
+                    ->collapsible(),
+            ])
+            ->columns([
+                Tables\Columns\TextColumn::make('type'),
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProviders::route('/'),
+            'create' => Pages\CreateProvider::route('/create'),
+            'edit' => Pages\EditProvider::route('/{record}/edit'),
+        ];
+    }
+}
